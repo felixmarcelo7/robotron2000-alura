5d88a23e247335d377f0ea556bb7f8d4d68b077c
felixmarcelo7 <felixmarcelo1998@gmail.com>
2023-06-13 23:28:32 -0300

    aula02

--- robotron-2000-projeto_inicial/robotron-2000-projeto_inicial/main.js
+++ robotron-2000-projeto_inicial/robotron-2000-projeto_inicial/main.js
@@ -1,5 +1,22 @@
-document.querySelector('#robotron').addEventListener('click', dizOi)
 
-function dizOi() {
-    console.log('oi');
+const controle = document.querySelectorAll("[data-controle]");
+
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    })
+})
+
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('[data-contador]');
+
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+        if(peca.value <= 0) {
+            peca.value = "00";
+        };
+    } else {
+        peca.value = parseInt(peca.value) + 1;
+    }
 }
